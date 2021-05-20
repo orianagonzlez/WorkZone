e8369f833be675a6240c21043a1ad8ff6c30c36d
RoccoMadonna <rocco.madonna@correo.unimet.edu.ve>
2021-05-20 19:17:47 -0400

    modal piedrero

--- work-zone-frontend/src/App.js
+++ work-zone-frontend/src/App.js
@@ -1,10 +1,22 @@
 import { Button } from "react-bootstrap";
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ModalNewProyect from "./ModalNewProyect";
 
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  const onClick = () => setShowModal(true);
+
+  //handleSubmit(name) => //some code
+
   return (
     <div className="m-5">
       <h1>WORK ZONE!!</h1>
-      <Button variant="info">Primary</Button>
+      <Button variant="info" onClick={onClick}>
+        {showModal ? <ModalNewProyect /> : null}
+        Show modal
+      </Button>
     </div>
   );
 }
