3daa24c83b7445d8a286861fb9839605067796e1
Guillermo Sosa <g.sosa@correo.unimet.edu.ve>
2021-05-22 10:57:51 -0400

    Drag and Drop en HomeScreen

--- work-zone-frontend/src/App.js
+++ work-zone-frontend/src/App.js
@@ -1,19 +1,18 @@
 import { AppRouter } from "./router/AppRouter";
 import { AppProvider } from "./context/AppContext";
+import { HomeScreen } from "./pages/HomeScreen";
 
 function App() {
   return (
-
     <div className="main-container">
-
         <AppProvider>
           <AppRouter />
         </AppProvider>
 
+        {/* <HomeScreen /> */}
+
       <div className="footer"></div>
-
     </div>
-
   );
 }
 
--- work-zone-frontend/src/pages/HomeScreen.js
+++ work-zone-frontend/src/pages/HomeScreen.js
@@ -1,9 +1,169 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { Container } from "react-bootstrap";
+
+const itemsFromBackend = [
+  {
+    id: "task1",
+    content: "Task 1",
+  },
+  {
+    id: "task2",
+    content: "Task 2",
+  },
+  {
+    id: "task3",
+    content: "Task 3",
+  },
+  {
+    id: "task4",
+    content: "Task 4",
+  },
+  {
+    id: "task5",
+    content: "Task 5",
+  },
+];
+
+const columnsFromBackend = {
+  ["column0"]: {
+    name: "Requested",
+    items: itemsFromBackend,
+  },
+  ["column1"]: {
+    name: "To do",
+    items: [],
+  },
+  ["column2"]: {
+    name: "In Progress",
+    items: [],
+  },
+  ["column3"]: {
+    name: "Done",
+    items: [],
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
 
 export const HomeScreen = () => {
-    return (
-        <div>
-            <h1>Home Screen</h1>
-        </div>
-    )
-}
+  const [columns, setColumns] = useState(columnsFromBackend);
+
+  return (
+
+    <Container className="componentContainer">
+      <h1>Tasks</h1>
+      <div
+        style={{ display: "flex", justifyContent: "center", height: "100%" }}
+      >
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500,
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#263B4A"
+                                          : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+              </div>
+            );
+          })}
+        </DragDropContext>
+      </div>
+    </Container>
+  );
+};
