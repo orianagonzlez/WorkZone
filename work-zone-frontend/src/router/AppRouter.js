01998dc77f05ac97bd56e02e98d4ceae02811690
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-05-25 10:18:06 -0400

    hotfix rutas

--- work-zone-frontend/src/router/AppRouter.js
+++ work-zone-frontend/src/router/AppRouter.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 
 import { AuthRouter } from "./AuthRouter";
 import { ProjectsRouter } from "./ProjectsRouter";
-import { ProjectDeetsRouter } from "./ProjectDeetsRouter";
 import { PrivateRoute } from "./PrivateRoute";
 
 import { PublicRoute } from "./PublicRoute";
 import { AppContext } from "../context/AppContext";
-import ProjectDeets from "../components/projects/projectDeets/ProjectDeets";
 
 export const AppRouter = () => {
   const { user } = useContext(AppContext);
@@ -28,19 +26,6 @@ export const AppRouter = () => {
             component={AuthRouter}
             isAuthenticated={user.isLogged}
           />
-
-          <PublicRoute
-            path="/projectDetails"
-            component={ProjectDeetsRouter}
-            isAuthenticated={user.isLogged}
-          />
-
-          {/* <PublicRoute
-                      exact
-                      isAuthenticated={user.isLogged}
-                      path="/projects/create"
-                      component={CreateProjectsRouter}
-                    /> */}
 
           <Redirect to="/auth/login" />
         </Switch>
--- work-zone-frontend/src/router/ProjectsRouter.js
+++ work-zone-frontend/src/router/ProjectsRouter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import ProjectsPage from "../pages/projects";
 import CreateProjectsScreen from "../pages/projects/CreateProjectsScreen";
+import ProjectDeetsPage from "../pages/projects/projectDeets";
 
 export const ProjectsRouter = () => {
-  console.log("projects");
   return (
     <div className="App">
       <div className="AppPage">
         <Switch>
+          <Route path="/projects/details" component={ProjectDeetsPage} />
           <Route path="/projects/create" component={CreateProjectsScreen} />
           <Route path="/projects" component={ProjectsPage} />
 
