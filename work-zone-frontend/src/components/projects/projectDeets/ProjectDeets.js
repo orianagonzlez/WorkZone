1a22bfebb14e561e54bade8ad1821c235660d4b7
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2021-05-24 22:43:05 -0400

    Plan cards

--- work-zone-frontend/src/components/projects/projectDeets/ProjectDeets.js
+++ work-zone-frontend/src/components/projects/projectDeets/ProjectDeets.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
+import { Link } from "react-router-dom";
 import { Container, Button } from 'react-bootstrap';
 import { AppContext } from '../../../context/AppContext';
 import { postData } from '../../../helpers/postData';
-import { FaEdit } from "react-icons/fa";
+import { FaEdit, FaArrowCircleLeft } from "react-icons/fa";
 import { Board } from './Board';
 
 export default function ProjectDeets({ project }) {
@@ -21,7 +22,18 @@ export default function ProjectDeets({ project }) {
                 <Button className="upperButton" id="newTask" onClick={ () => {handleCreateTask()}}>+ Agregar Tarea</Button>
                 <Button className="upperButton" id="editProject" onClick={ () => {handleEditProject()}}><FaEdit /> Editar Proyecto</Button>
             </div>
-            <h1>Project Name{/* project.name */}</h1>
+            
+            <div className="divArrowLeft">
+                <div>
+                <Link to="/">
+                    <Button className="arrowLeft">
+                    <FaArrowCircleLeft />
+                    </Button>
+                </Link>
+                </div>
+                <h1>Project Name{/* project.name */}</h1>
+            </div>
+
             <Board />            
         </Container>
     )
