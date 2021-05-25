e8a1b60bc9f3f2971a18c550ceb807d169b9a4ad
RoccoMadonna <rocco.madonna@correo.unimet.edu.ve>
2021-05-24 23:54:18 -0400

    vista detalle proyecto continuada

--- work-zone-frontend/src/components/projects/projectDeets/ProjectDeets.js
+++ work-zone-frontend/src/components/projects/projectDeets/ProjectDeets.js
@@ -1,28 +1,75 @@
-import React, { useContext } from 'react';
-import { Container, Button } from 'react-bootstrap';
-import { AppContext } from '../../../context/AppContext';
-import { postData } from '../../../helpers/postData';
+import React, { useContext } from "react";
+import { Container, Button } from "react-bootstrap";
+import { AppContext } from "../../../context/AppContext";
+import { postData } from "../../../helpers/postData";
 import { FaEdit } from "react-icons/fa";
-import { Board } from './Board';
+import { Board } from "./Board";
+import { FaUsers } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaArrowCircleLeft } from "react-icons/fa";
 
 export default function ProjectDeets({ project }) {
-    //const {setUser, user} = useContext(AppContext);
+  //const {setUser, user} = useContext(AppContext);
 
-    const handleCreateTask = () => {
+  const handleCreateTask = () => {};
+  const handleEditProject = () => {};
 
-    };
-    const handleEditProject = () => {
+  return (
+    <Container fluid className="componentContainer">
+      <div className="upperButtons">
+        <Button
+          className="upperButton"
+          id="newTask"
+          onClick={() => {
+            handleCreateTask();
+          }}
+        >
+          + Agregar Tarea
+        </Button>
+        <Button
+          className="upperButton"
+          id="editProject"
+          onClick={() => {
+            handleEditProject();
+          }}
+        >
+          <FaEdit /> Editar Proyecto
+        </Button>
+      </div>
+      <div className="divArrowLeft">
+        <div>
+          <Link to="/">
+            <Button className="arrowLeft">
+              <FaArrowCircleLeft />
+            </Button>
+          </Link>
+        </div>
+        <div>
+          <h1>Project Name{/* project.name */}</h1>
+        </div>
+      </div>
 
-    };
+      <div className="miembros-logo">
+        <FaUsers /> Miembros
+      </div>
+      <div className="miembrosProyecto">
+        <div className="miembro">
+          <img src="logo192.png" className="profilePicture"></img>
+          <h3>Rocco Madonna</h3>
+        </div>
+        <div className="miembro">
+          <img src="logo192.png" className="profilePicture"></img>
+          <h3>Christoforo Sting</h3>
+        </div>
+        <div className="miembro">
+          <img src="logo192.png" className="profilePicture"></img>
+          <h3>Elque</h3>
+        </div>
+      </div>
 
-    return (
-        <Container fluid className="componentContainer">
-            <div className="upperButtons">
-                <Button className="upperButton" id="newTask" onClick={ () => {handleCreateTask()}}>+ Agregar Tarea</Button>
-                <Button className="upperButton" id="editProject" onClick={ () => {handleEditProject()}}><FaEdit /> Editar Proyecto</Button>
-            </div>
-            <h1>Project Name{/* project.name */}</h1>
-            <Board />            
-        </Container>
-    )
+      
+
+      <Board />
+    </Container>
+  );
 }
