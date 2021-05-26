c12cbe64d2165c8fe8c6e4cf55b46fa30e79f035
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-05-26 11:08:02 -0400

    el bug de la tabla

--- work-zone-frontend/src/components/projects/ProjectTable.js
+++ work-zone-frontend/src/components/projects/ProjectTable.js
@@ -1,13 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { FaTag, FaThList, FaUsers, FaChartLine, FaArchive } from 'react-icons/fa';
-import { useHistory } from 'react-router';
-import { AppContext } from '../../context/AppContext';
-import { getData } from '../../helpers/getData';
-import ProjectCard from './ProjectCard';
+import React, { useContext, useEffect, useState } from "react";
+import {
+  FaTag,
+  FaThList,
+  FaUsers,
+  FaChartLine,
+  FaArchive,
+  FaArrowCircleRight,
+} from "react-icons/fa";
+import { useHistory } from "react-router";
+import { AppContext } from "../../context/AppContext";
+import { getData } from "../../helpers/getData";
+import ProjectCard from "./ProjectCard";
 
-export const ProjectTable = ({show}) => {
-
-  const {setUser, user} = useContext(AppContext);
+export const ProjectTable = ({ show }) => {
+  const { setUser, user } = useContext(AppContext);
   const history = useHistory();
 
   const [projects, setProjects] = useState([]);
@@ -15,48 +21,60 @@ export const ProjectTable = ({show}) => {
   useEffect(() => {
     //Una vez que se tenga el id del usuario, se buscan los proyectos donde participa
     if (user?.id) {
-      getData(`https://workzone-backend-mdb.herokuapp.com/api/projects/by/${user.id}`).then( r => {
-      console.log('me respondio' + r);
-      if (r.ok) {
-        setProjects(r.data);
-      } else {
-        console.log('error');
-      }
+      getData(
+        `https://workzone-backend-mdb.herokuapp.com/api/projects/by/${user.id}`
+      ).then((r) => {
+        console.log("me respondio" + r);
+        if (r.ok) {
+          setProjects(r.data);
+        } else {
+          console.log("error");
+        }
+      });
     }
-    )
-    }
-    
   }, [user, show]);
-  
+
   return (
     <div>
       {/* Los style estan en _preview.scss */}
-        <div className="Preview__container"> 
-            <ul className="Preview__responsive-table">
-              <li className="Preview__table-header">
-                <div className="column column-3 "><FaTag /> Nombre</div>
-                <div className="column column-3 "><FaThList /> Tareas</div>
-                <div className="column column-3 "><FaUsers /> Miembros</div>
-                <div className="column column-3"><FaChartLine /> Progreso</div>
-                <div className="column column-3"><FaArchive /> {show ? 'Devolver': 'Archivar'} </div>
-              </li>
-              
-              {projects.map((project) => {
-                if (!show && !project.archivado) {
-                  return (
-                    <li className="Preview__table-row" key={project._id} onClick={() => history.push(`/projects/details/${project._id}`)}>
-                      <ProjectCard project={project}/> 
-                    </li>
-                  )}
-                if (show && project.archivado) {
-                  return (
-                    <li className="Preview__table-row" key={project._id} onClick={() => history.push(`/projects/details/${project._id}`)}>
-                      <ProjectCard project={project} /> 
-                    </li>
-                  )
-                }})}
-            </ul>
-          </div>
+      <div className="Preview__container">
+        <ul className="Preview__responsive-table">
+          <li className="Preview__table-header">
+            <div className="column column-4 ">
+              <FaTag /> Nombre
+            </div>
+            <div className="column column-3 ">
+              <FaThList /> Tareas
+            </div>
+            <div className="column column-3">
+              <FaChartLine /> Progreso
+            </div>
+            <div className="column column-3">
+              <FaArchive /> {show ? "Devolver" : "Archivar"}{" "}
+            </div>
+            <div className="column column-2 ">
+              <FaArrowCircleRight /> Detalles
+            </div>
+          </li>
+
+          {projects.map((project) => {
+            if (!show && !project.archivado) {
+              return (
+                <li className="Preview__table-row" key={project._id}>
+                  <ProjectCard project={project} />
+                </li>
+              );
+            }
+            if (show && project.archivado) {
+              return (
+                <li className="Preview__table-row" key={project._id}>
+                  <ProjectCard project={project} setProjects={setProjects} />
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
