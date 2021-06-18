6328047c7d81886cc2bcc9cd7b30d5f2d93d5bd6
Oriana Gonzalez <go.oriana@correo.unimet.edu.ve>
2021-06-18 17:23:09 -0400

    quitando console log

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -8,7 +8,6 @@ export const useForm = (initialState = {}) => {
   };
 
   const handleInputChange = ({ target }) => {
-    console.log(target);
     setValues({
       ...values,
       [target.name]: target.value,
