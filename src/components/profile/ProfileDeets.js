b7040624a418fb5c6a72224ec8803e76eca17e7f
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-06-30 09:57:54 -0400

    actuslizar contrasena

--- src/components/profile/ProfileDeets.js
+++ src/components/profile/ProfileDeets.js
@@ -4,16 +4,17 @@ import { FaEdit, FaLockOpen, FaUserCircle } from "react-icons/fa";
 import { AppContext } from "../../context/AppContext";
 import { getData } from "../../helpers/getData";
 import { EditProfileModal } from "./EditProfileModal";
+import Swal from "sweetalert2";
+import { UpdatePasswordModal } from "./UpdatePasswordModal";
 
 export default function ProfileDeets() {
-  
   const { setUser, user } = useContext(AppContext);
   const [usuario, setUsuario] = useState({});
   const [modalShow, setModalShow] = useState(false);
+  const [passwordModal, setPasswordModal] = useState(false);
 
   useEffect(() => {
-  
-    if(user?.id) {
+    if (user?.id) {
       getData(
         `https://workzone-backend-mdb.herokuapp.com/api/auth/${user.id}`
       ).then((r) => {
@@ -25,29 +26,48 @@ export default function ProfileDeets() {
         }
       });
     }
-     
   }, [user, modalShow]);
 
   const handleEditProfile = () => {
     setModalShow(true);
-  }
- 
+  };
+
+  const handleUpdatePassword = () => {
+    setPasswordModal(true);
+  };
 
   return (
     <Container fluid className="profileDeetsContainer">
       <div className="upperButtonsProfile">
-        <Button className="upperButtonProfile" id="changePass">
+        <Button
+          onClick={() => handleUpdatePassword()}
+          className="upperButtonProfile"
+          id="changePass"
+        >
           <FaLockOpen /> Cambiar Contraseña
         </Button>
-
-        <Button className="upperButtonProfile" id="editProfile" onClick={() => handleEditProfile()}>
+        <Button
+          className="upperButtonProfile"
+          id="editProfile"
+          onClick={() => handleEditProfile()}
+        >
           <FaEdit /> Editar Perfil
         </Button>
-       {usuario?.uid &&  <EditProfileModal
-        usuario={usuario}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        />}
+        {usuario?.uid && (
+          <EditProfileModal
+            usuario={usuario}
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        )}
+        {usuario?.uid && (
+          <UpdatePasswordModal
+            show={passwordModal}
+            onHide={() => setPasswordModal(false)}
+          />
+        )}
+
+        <UpdatePasswordModal />
       </div>
 
       <div className="gridContainer">
@@ -56,29 +76,29 @@ export default function ProfileDeets() {
             <div className="nombre">
               <div className="profileLabel" id="name">
                 <h2>Nombre</h2>
-                <h3>{ usuario.nombre }</h3>
+                <h3>{usuario.nombre}</h3>
               </div>
 
               <div className="profileLabel" id="lastname">
                 <h2>Apellido</h2>
-                <h3>{ usuario.apellido }</h3>
+                <h3>{usuario.apellido}</h3>
               </div>
             </div>
             <div className="moreInfo">
               <div className="profileLabel" id="email">
                 <h2>Correo electronico</h2>
-                <h3>{ usuario.email }</h3>
+                <h3>{usuario.email}</h3>
               </div>
 
               <div className="profileLabel" id="username">
                 <h2>Nombre de usuario</h2>
-                <h3>{ usuario.username }</h3>
+                <h3>{usuario.username}</h3>
               </div>
             </div>
             <div className="">
               <div className="profileLabel" id="birth">
                 <h2>Fecha de nacimiento</h2>
-                <h3>{ usuario.fechaNacimiento?.split('T')[0] }</h3>
+                <h3>{usuario.fechaNacimiento?.split("T")[0]}</h3>
               </div>
             </div>
           </Container>
---
+++ src/components/profile/UpdatePasswordModal.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { Modal, Form, Col, Alert } from "react-bootstrap";
+import { AppContext } from "../../context/AppContext";
+import { postData } from "../../helpers/postData";
+import { useForm } from "../../hooks/useForm";
+import Swal from "sweetalert2";
+
+export const UpdatePasswordModal = ({ show, onHide }) => {
+  const [formValues, handleInputChange, reset] = useForm({
+    password1: "",
+    password2: "",
+  });
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const { password1, password2 } = formValues;
+
+  const { setUser, user } = useContext(AppContext);
+
+  const handleUpdatePassword = (e) => {
+    e.preventDefault();
+
+    if (password1 !== password2) {
+      setErrorMsg("Las contraseñas no coinciden");
+      return;
+    }
+    if (password1.length < 6) {
+      setErrorMsg("La contraseñas debe tener al menos 6 carcateres");
+      return;
+    }
+
+    const body = {
+      id: user.id,
+      contrasena: password1,
+    };
+
+    postData(
+      "https://workzone-backend-mdb.herokuapp.com/api/auth/updatePassword",
+      body
+    ).then((r) => {
+      console.log("me respondio" + r);
+      if (r.ok) {
+        console.log(r.data);
+
+        Swal.fire({
+          icon: "success",
+          title: "Contraseña actualizada!",
+          text:
+            "La proxima vez que inicies sesión hazlo con tu nueva contraseña",
+          confirmButtonColor: "#22B4DE",
+        });
+
+        onHide();
+      } else {
+        console.log("error");
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Se produjo un error, intenta de nuevo",
+          confirmButtonColor: "#22B4DE",
+        });
+      }
+    });
+  };
+
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={onHide}
+        backdrop="static"
+        keyboard={false}
+        centered
+        animation={false}
+      >
+        <Form className="login_form" onSubmit={handleUpdatePassword}>
+          <Modal.Header closeButton>
+            <Modal.Title>Cambio de contraseña</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Row className="d-flex align-items-center justify-content-start my-3 mx-5 px-5">
+              <Form.Group as={Col}>
+                <Form.Label>Nueva contraseña</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="password"
+                  name="password1"
+                  autoComplete="off"
+                  value={password1}
+                  onChange={handleInputChange}
+                  required
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="d-flex align-items-center justify-content-start my-3 mx-5 px-5">
+              <Form.Group as={Col}>
+                <Form.Label>Confirmar nueva contraseña</Form.Label>
+                <Form.Control
+                  className="input"
+                  type="password"
+                  autoComplete="off"
+                  name="password2"
+                  value={password2}
+                  onChange={handleInputChange}
+                  required
+                />
+              </Form.Group>
+            </Form.Row>
+            {errorMsg && (
+              <div className="alert alert-danger my-3" role="alert">
+                {errorMsg}
+              </div>
+            )}
+
+            <div className="button ">
+              <button className="button_modal" type="submit">
+                Guardar
+              </button>
+            </div>
+          </Modal.Body>
+        </Form>
+      </Modal>
+    </>
+  );
+};
